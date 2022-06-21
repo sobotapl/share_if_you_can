5f246d829a377c1b8d0a0926ad3b332b672c7ad3
sobotapl <piosoboci@gmail.com>
2022-06-21 21:15:29 +0200

    findAll method in InstitionRepository added

--- src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
@@ -1,8 +1,15 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.model.Institution;
 
+import java.util.List;
+
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+
+    @Query("SELECT i FROM Institution i ORDER BY i.id DESC")
+    List<Institution> findAllById();
+
 }
