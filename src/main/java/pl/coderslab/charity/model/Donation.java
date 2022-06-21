2bac46affbd2316d7c9c5132423e2a226a4db07a
sobotapl <piosoboci@gmail.com>
2022-06-21 20:08:24 +0200

    Donation Repository added

---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,50 @@
+package pl.coderslab.charity.model;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@Table(name = "donations")
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int quantity;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    @ManyToOne
+    private Institution institution;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String zipCode;
+
+    @NotNull
+    private LocalDate pickUpDate;
+
+    @NotNull
+    private LocalTime pickUpTime;
+
+    @NotNull
+    private String pickUpComment;
+}
---
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -0,0 +1,8 @@
+package pl.coderslab.charity.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.charity.model.Category;
+import pl.coderslab.charity.model.Donation;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+}
